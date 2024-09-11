91344c12bc747a7fc84a000184f5cedcb360a7af
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2024-09-10 22:28:44 -0500

    Healthcheck URL

--- main/__main__.py
+++ main/__main__.py
@@ -1,5 +1,6 @@
 import json
 
+import requests
 from pymongo import MongoClient
 
 from main.checkin_util import CheckinUtil
@@ -14,6 +15,7 @@ def main():
     db_password = config["db_password"]
     db_host = config["db_host"]
     ut_username = config["ut_username"]
+    healthcheck_url = config["healthcheck_url"]
 
     uri = f"mongodb+srv://{db_username}:{db_password}@{db_host}/?retryWrites=true&w=majority"
     client = MongoClient(uri)
@@ -25,6 +27,9 @@ def main():
     checkin_util.backup_recent_beers()
 
     print(f"There are now {collection.count_documents({})} beer documents")
+
+    requests.get(healthcheck_url)
+    print(f"Pinged {healthcheck_url}")
 
 
 if __name__ == '__main__':
